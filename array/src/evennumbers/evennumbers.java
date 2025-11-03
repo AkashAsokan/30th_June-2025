8d794ed740c1553b90a09e102e7f8d16c42cf9f3
AkashAsokan <akashasokan112003@gmail.com>
2025-11-03 16:48:53 +0530

    test1

---
+++ array/src/evennumbers/evennumbers.java
@@ -0,0 +1,15 @@
+package evennumbers;
+
+public class evennumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Main Starts");
+		if(26%5==0)
+		{
+			System.out.println("25 is Multiple of 5");
+		}
+		System.out.println(" Main ends");
+		
+	}
+
+}
